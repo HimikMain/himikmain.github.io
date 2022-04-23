504da17bd02e479b1e4408324307dd8f647b4dd2
HimikMain <himikmainz@gmail.com>
2022-04-23 22:28:16 +0300

    AntonGL

---
+++ anton/src/index.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
+
+//Header временно
+   $(window).on('load', function () {
+        $("#header-scroll").removeClass("small")
+  });
+
+$(window).scroll(function () {
+     var sc = $(window).scrollTop()
+    if (sc > 1) {
+        $("#header-scroll").addClass("small")
+    } else {
+        $("#header-scroll").removeClass("small")
+    }
+});
+
+//scrollspy
+$(window).on('scroll', function () {
+   var sections = $('section')
+    , nav = $('nav')
+    , nav_height = nav.outerHeight()
+    , cur_pos = $(this).scrollTop();
+  sections.each(function() {
+    var top = $(this).offset().top - nav_height,
+        bottom = top + $(this).outerHeight();
+ 
+    if (cur_pos >= top && cur_pos <= bottom) {
+      nav.find('a').removeClass('active');
+      sections.removeClass('active');
+ 
+      $(this).addClass('active');
+      nav.find('a[href="#'+$(this).attr('id')+'"]').addClass('active');
+    }
+  });
+});
